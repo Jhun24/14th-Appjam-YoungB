d54be36c325cf3fe6a183e9f1d587b112b9cb74b
Jhun24 <jjanghuni102@naver.com>
2017-12-17 01:47:35 +0900

    update

--- routes/auth.js
+++ routes/auth.js
@@ -3,55 +3,103 @@
  */
 
 var passport = require('passport');
-var LocalStrategy = require('passport-local').Strategy;
-var FacebookStrategy = require('passport-facebook').Strategy;
-
+var FacebookTokenStrategy = require('passport-facebook-token');
 
 module.exports = auth;
 
 function auth(app , randomstring , userModel) {
 
-    passport.serializeUser((user, done)=>{
-        console.log("serialize")
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    passport.serializeUser(function(user, done){
         done(null, user);
     });
 
-    passport.deserializeUser((user, done)=>{
-        console.log("deserialize")
-        done(null, user);
+    passport.deserializeUser(function (obj, done) {
+        done(null, obj);
     });
 
-    passport.use(new FacebookStrategy({
-        clientID: '552505858435061',
-        clientSecret: 'aeb29558f28b1f8507f9b4f8f9c6b848',
-        callbackURL: 'soylatte.kr:6974/auth/facebook/callback',
-        passReqToCallback: true,
-    }, (req, accessToken, refreshToken, profile, done) => {
-        userModel.findOne({ id: profile.id }, (err, user) => {
-            if (user) {
-                return done(err, user);
+    passport.use(new FacebookTokenStrategy({
+        clientID : "552505858435061",
+        clientSecret : "aeb29558f28b1f8507f9b4f8f9c6b848"
+    }, function (accessToken, refreshToken, profile, done) {
+        console.log(profile);
+        userModel.findOne({
+            id : profile.id
+        }, function (err, user) {
+            if(err){
+                return done(err);
             }
-            var token = randomstring.generate();
-            var saveUser = new userModel({
-                id: profile.id,
-                name: profile.name,
-                password : profile.password,
-                token : token,
-            });
-            saveUser.save((err) => {
-                if(err) throw err;
-                return done(200 , token);
-            });
-        });
+            if(!user){
+                var saveUser = new userModel({
+                    id : profile.id,
+                    name : profile.displayName,
+                });
+                saveUser.save(function (err) {
+                    if(err) console.log(err);
+                    else{
+                        done(null, profile);
+                    }
+                })
+            }
+            else if(user){
+                done(null, profile);
+            }
+        })
     }));
 
+    app.get('/auth/facebook/token', passport.authenticate('facebook-token'), function (req, res) {
+        console.log("user token : " + req.param('access_token'));
+        if(req.user){
+            var response = {
+                id : req.user.id,
+                name : req.user.displayName
 
-    app.get('/auth/facebook', passport.authenticate('facebook', {
-        authType: 'rerequest', scope: ['public_profile', 'email']
-    }));
-    app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), function(req, res) {
-        res.redirect('/');
+            };
+            res.send(200, response);
+        }
+        else if(!req.user){
+            res.send(401, "Can't find User On Facebook. It May Be Unusable.");
+        }
     });
+
+    app.get('/auth/facebook/callback', passport.authenticate('facebook-token', {
+        successRedirect : '/',
+        failureRedirect : '/'
+    }));
+
+    // passport.use(new FacebookStrategy({
+    //     clientID: '552505858435061',
+    //     clientSecret: 'aeb29558f28b1f8507f9b4f8f9c6b848',
+    //     callbackURL: 'soylatte.kr:6974/auth/facebook/callback',
+    //     passReqToCallback: true,
+    // }, (req, accessToken, refreshToken, profile, done) => {
+    //     userModel.findOne({ id: profile.id }, (err, user) => {
+    //         if (user) {
+    //             return done(err, user);
+    //         }
+    //         var token = randomstring.generate();
+    //         var saveUser = new userModel({
+    //             id: profile.id,
+    //             name: profile.name,
+    //             password : profile.password,
+    //             token : token,
+    //         });
+    //         saveUser.save((err) => {
+    //             if(err) throw err;
+    //             return done(200 , token);
+    //         });
+    //     });
+    // }));
+    //
+    //
+    // app.get('/auth/facebook', passport.authenticate('facebook', {
+    //     authType: 'rerequest', scope: ['public_profile', 'email']
+    // }));
+    // app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/' }), function(req, res) {
+    //     res.redirect('/');
+    // });
 
     app.post('/auth/login',(req,res)=>{
         "use strict";
